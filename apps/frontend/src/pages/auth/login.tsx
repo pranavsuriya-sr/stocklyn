1498697d6e6e3e5a38f958af567796fb966a4022
Uday <udayraj.vadeghar@gmail.com>
2025-01-02 14:59:02 +0000

    add form components and dependencies for improved form handling

--- apps/frontend/src/pages/auth/login.tsx
+++ apps/frontend/src/pages/auth/login.tsx
@@ -1,45 +1,120 @@
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { createClient, Session } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-// Replace placeholders with actual Supabase project details
-const supabase = createClient(
+const supabaseClient = createClient(
   "https://hqenglckisxxnilbjpim.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZW5nbGNraXN4eG5pbGJqcGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MDk0NDIsImV4cCI6MjA1MTI4NTQ0Mn0.0c0EPJVLBQUBVdXyWbCqrQkYRiOuU9yjNTKjCkr-Y-s"
 );
 
-export default function Login() {
-  const [session, setSession] = useState<Session | null>(null);
+const loginSchema = z.object({
+  username: z
+    .string()
+    .nonempty()
+    .min(3, { message: "username must have at least 3 characters" })
+    .max(20, { message: "username must have at most 20 characters" }),
+  email: z.string().email({ message: "Enter a valid email" }).nonempty(),
+  password: z
+    .string()
+    .nonempty()
+    .min(6, { message: "password must have at least 6 characters" }),
+});
 
+const Login = () => {
+  const [session, setSession] = useState<Session | null>(null);
   useEffect(() => {
-    // Fetch the current session
-    supabase.auth.getSession().then(({ data: { session } }) => {
+    supabaseClient.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
     });
-
-    // Subscribe to authentication state changes
-    const { data: subscription } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setSession(session);
-      }
-    );
-
-    // Cleanup subscription on component unmount
-    return () => {
-      subscription.subscription?.unsubscribe();
-    };
   }, []);
 
+  const loginForm = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit() {
+    console.log("submit");
+  }
+
+  //not logged in
   if (!session) {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="p-6 bg-white rounded shadow-md">
-          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-        </div>
+      <div className="flex justify-center items-center h-screen ">
+        <Form {...loginForm}>
+          <form
+            onSubmit={loginForm.handleSubmit(onSubmit)}
+            className="space-y-2 border-black border rounded-md p-6"
+          >
+            <FormField
+              control={loginForm.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormDescription>This is your Email.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormDescription>This is your password.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     );
-  } else {
-    return <div>Logged in!</div>;
   }
-}
+
+  return <div>login</div>;
+};
+
+export default Login;
