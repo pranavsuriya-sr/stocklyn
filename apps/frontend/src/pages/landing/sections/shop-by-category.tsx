1bb767744701d9dfd54d373ecde471ce001f2c6b
Uday <udayraj.vadeghar@gmail.com>
2025-01-03 15:19:10 +0000

    enhance ShopByCategory section with new layout and images

--- apps/frontend/src/pages/landing/sections/shop-by-category.tsx
+++ apps/frontend/src/pages/landing/sections/shop-by-category.tsx
@@ -1,12 +1,29 @@
 const ShopByCategory = () => {
   return (
-    <div className="flex">
-      <div className="flex flex-col h-screen w-1/2 justify-center">
-        <div>left</div>
+    <div>
+      <div className="flex justify-between mt-10 p-10">
+        <span className="text-3xl font-bold font-mono">Shop here</span>
+        <span className="Shop here ">
+          <button className="text-violet-700">browse all categories→</button>
+        </span>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <div>1</div>
-        <div>2</div>
+
+      <div className="flex mt-10 pt-10 bg-gray-100">
+        <div className="flex flex-col h-screen w-1/2 justify-center items-center ">
+          <img
+            src="https://i.imgur.com/KjOYhYn.jpeg"
+            className="transition-transform duration-300 ease-in-out hover:brightness-75 h-auto w-3/4 rounded-md hover:shadow-2xl "
+          ></img>
+          <span className="text-3xl text-gray-600 font-semibold">
+            New Arrivals
+          </span>
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src="https://i.imgur.com/7p9nZpJ.jpeg"
+            className=" transition-transform duration-300 ease-in-out hover:brightness-75 border rounded-xl hover:shadow-2xl"
+          ></img>
+        </div>
       </div>
     </div>
   );
