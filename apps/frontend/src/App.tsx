b3d3aa7067c4732f29d6ad01e52cad694f8f3076
Uday <udayraj.vadeghar@gmail.com>
2025-01-03 10:28:35 +0000

    add LandingPage component and update routing to include it

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/login";
 import SignUp from "./pages/auth/sign-up";
 import Home from "./pages/home/home";
+import LandingPage from "./pages/landing/landing-page";
 import ProtectedPage from "./pages/protected/ProtectedPage";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           </ProtectedPage>
         }
       />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
---
+++ apps/frontend/src/pages/landing/landing-page.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <div>landingpage</div>;
+};
+
+export default LandingPage;
