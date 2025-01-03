c475fbf1cdcc38c18a2b9204b560a01fd5697bab
Uday <udayraj.vadeghar@gmail.com>
2025-01-03 14:33:01 +0000

    add ShopByCategory section to LandingPage and adjust HeroSection layout

--- apps/frontend/src/pages/landing/landing-page.tsx
+++ apps/frontend/src/pages/landing/landing-page.tsx
@@ -1,6 +1,12 @@
 import HeroSection from "./sections/hero-section";
+import ShopByCategory from "./sections/shop-by-category";
 
 const LandingPage = () => {
-  return <HeroSection />;
+  return (
+    <div className="w-[90%] mx-auto border-2">
+      <HeroSection />
+      <ShopByCategory />
+    </div>
+  );
 };
 export default LandingPage;
---
+++ apps/frontend/src/pages/landing/sections/shop-by-category.tsx
@@ -0,0 +1,15 @@
+const ShopByCategory = () => {
+  return (
+    <div className="flex">
+      <div className="flex flex-col h-screen w-1/2 justify-center">
+        <div>left</div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopByCategory;
