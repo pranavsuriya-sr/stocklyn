2e750f8c82172ba0da1e6e5c21b76f9d5a62e90b
Uday <udayraj.vadeghar@gmail.com>
2025-01-04 17:58:42 +0000

    add zustand && update home page

--- apps/frontend/src/components/navbar/navbar-components/view-profile.tsx
+++ apps/frontend/src/components/navbar/navbar-components/view-profile.tsx
@@ -37,7 +37,7 @@ const ViewProfile = () => {
           sideOffset={4}
         >
           <DropdownMenuLabel className="text-lg">
-            {displayName} 😄
+            {displayName} Hello!😄
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>Profile</DropdownMenuItem>
