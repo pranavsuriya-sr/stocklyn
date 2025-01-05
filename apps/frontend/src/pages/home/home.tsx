e448a42cbd56c5f897f47a73e9c6fe23c32e221c
Uday <udayraj.vadeghar@gmail.com>
2025-01-05 18:25:38 +0000

    add fetching logic to home page & update prisma schema

--- apps/frontend/src/pages/home/home.tsx
+++ apps/frontend/src/pages/home/home.tsx
@@ -1,131 +1,75 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-02.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 3,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-03.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 4,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-04.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 5,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 6,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-04.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 7,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-02.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-  {
-    id: 8,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-03.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "₹399",
-    color: "Black",
-  },
-];
+interface Products {
+  id: string;
+  name: string;
+  imageUrl: string[];
+  price: number;
+  productDescription: string;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<Products[]>([]);
+
+  const FetchProductLists = async () => {
+    try {
+      const productDetails = await axios.get(import.meta.env.VITE_ALL_PRODUCTS);
+      setProducts(productDetails.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    FetchProductLists();
+  }, []);
+  console.log("fetched details " + products);
+
   const navigate = useNavigate();
 
-  function HandleProductClick(productId: Number) {
+  function HandleProductClick(productId: string) {
     navigate(`/product/${productId}`);
   }
 
   return (
-    <div className="bg-white pt-20">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Our best collection
-        </h2>
+    products && (
+      <div className="bg-white pt-20">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our best collection
+          </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="group relative"
-              onClick={() => HandleProductClick(product.id)}
-            >
-              <img
-                alt={product.imageAlt}
-                src={product.imageSrc}
-                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
-              />
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className="group relative"
+                onClick={() => HandleProductClick(product.id)}
+              >
+                <img
+                  alt={product.name}
+                  src={product.imageUrl[0]}
+                  className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+                />
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                    </a>
-                  </h3>
-                  <p className="text-sm font-medium text-gray-900">
-                    {product.price}
-                  </p>
+                    </h3>
+                    <p className="text-sm font-medium text-gray-900">
+                      {product.price}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
