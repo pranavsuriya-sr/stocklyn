c0755f09f76757c0012394d24492ea3e51bd996b
Uday <udayraj.vadeghar@gmail.com>
2025-01-07 17:42:16 +0000

    refactor Prisma schema and add user details endpoint in server

--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -1,13 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
 import { productRoute } from "./routes/product-route/product-route";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use("/product", productRoute);
 
 const PORT = 5000;
+
+app.get("/", async (req, res) => {
+  try {
+    const userDetails = await prisma.$queryRaw`SELECT * FROM auth.users`;
+
+    res.send(userDetails);
+  } catch (error) {}
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
