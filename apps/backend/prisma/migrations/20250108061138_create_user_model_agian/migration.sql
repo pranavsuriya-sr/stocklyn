0f79c90dffba27109e3ef877c14afb2a26ab1970
Uday <udayraj.vadeghar@gmail.com>
2025-01-08 06:43:13 +0000

    refactor: remove old users model and update migrations for new user structure

---
+++ apps/backend/prisma/migrations/20250108061138_create_user_model_agian/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `hashedPassword` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "hashedPassword",
+DROP COLUMN "updatedAt";
