dc5b3732064f4bc66ce3fd78490de355950e2f5d
Uday <udayraj.vadeghar@gmail.com>
2025-01-09 10:09:17 +0000

    add login route with user authentication

--- apps/backend/src/routes/auth/auth.route.ts
+++ apps/backend/src/routes/auth/auth.route.ts
@@ -3,13 +3,14 @@ import bcrypt from "bcryptjs";
 import express from "express";
 
 const authRoute = express.Router();
+
 const prismaClient = new PrismaClient();
 
 authRoute.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    res.send(400).json({ error: "Missing required fields" });
+    res.status(400).json({ error: "Missing required fields" });
     return;
   }
   if (password.length < 6) {
@@ -61,6 +62,63 @@ authRoute.post("/signup", async (req, res) => {
 
     res.status(500).json({
       error: "Failed to create user",
+    });
+    return;
+  } finally {
+    await prismaClient.$disconnect();
+  }
+});
+
+authRoute.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      error: "Missing required fields",
+    });
+  }
+
+  try {
+    const user = await prismaClient.users.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        error: "User not found",
+      });
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!passwordMatch) {
+      res.status(401).json({
+        error: "Invalid Credentials",
+      });
+      return;
+    }
+
+    const userWithoutPassword = { ...user, hashedPassword: undefined };
+
+    res.status(200).json({
+      message: "Login successful!",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        res.status(409).json({
+          error: "A user with this email doesnt exist",
+        });
+        return;
+      }
+    }
+
+    res.status(500).json({
+      error: "Failed to login",
     });
     return;
   } finally {
