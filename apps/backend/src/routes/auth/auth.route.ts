02a7fed0c4142ae678513fd13674ef1804899b0a
Uday <udayraj.vadeghar@gmail.com>
2025-01-09 08:35:16 +0000

    add authentication route with password hashing and update dependencies

--- apps/backend/src/routes/auth/auth.route.ts
+++ apps/backend/src/routes/auth/auth.route.ts
@@ -1,1 +1,21 @@
+import bcrypt from "bcryptjs";
+import express from "express";
 
+const authRoute = express.Router();
+
+authRoute.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    res.send(hashedPassword);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send(error);
+    }
+  }
+});
+
+export default authRoute;
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
+import authRoute from "./routes/auth/auth.route";
 import { productRoute } from "./routes/product/product.route";
 
 const app = express();
+
 const prisma = new PrismaClient();
 
 app.use(cors());
+app.use(express.json());
 app.use("/product", productRoute);
+app.use("/auth", authRoute);
 
 const PORT = 5000;
 
