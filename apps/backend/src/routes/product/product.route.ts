4532dd2f694d30788842c82cde0c0f1e9b738bba
Uday <udayraj.vadeghar@gmail.com>
2025-01-17 09:49:11 +0000

    add product addition route with validation and error handling

--- apps/backend/src/routes/product/product.route.ts
+++ apps/backend/src/routes/product/product.route.ts
@@ -4,7 +4,9 @@ import { VerifyJwtMiddleware } from "../../middleware/verify-jwt";
 
 export const productRoute = express.Router();
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
 
 productRoute.get("/info", VerifyJwtMiddleware, async (req, res) => {
   try {
@@ -19,12 +21,10 @@ productRoute.post("/productDetails", VerifyJwtMiddleware, async (req, res) => {
   const { productIds } = req.body;
 
   if (!productIds) {
-    res
-      .status(400)
-      .json({
-        message:
-          "Required feild is missing , ie productIds[] at /product/productDetails",
-      });
+    res.status(400).json({
+      message:
+        "Required feild is missing , ie productIds[] at /product/productDetails",
+    });
     return;
   }
 
@@ -37,5 +37,51 @@ productRoute.post("/productDetails", VerifyJwtMiddleware, async (req, res) => {
     res.status(200).json(productDetails);
   } catch (error) {
     res.status(401).json(error.message);
+  }
+});
+
+//enable jwtverification later
+productRoute.post("/add", async (req, res) => {
+  const {
+    name,
+    imageUrl,
+    productDescription = "",
+    category,
+    price,
+    details = "",
+    highlights = [],
+    stockQuantity = 0,
+    colors,
+    displayImage,
+  } = req.body;
+
+  if (!name || !category || !imageUrl || !price || !colors || !displayImage) {
+    res.status(400).json({
+      message:
+        "Required feilds are missing , ie (id,name,imageUrl,category,imageUrl[],price,colors,displayImage) at /product/add",
+    });
+    return;
+  }
+
+  try {
+    const response = await prisma.products.create({
+      data: {
+        name,
+        imageUrl,
+        productDescription,
+        category,
+        price,
+        details,
+        highlights,
+        reviews: 0,
+        stockQuantity,
+        colors,
+        displayImage,
+      },
+    });
+
+    res.status(201).json({ message: "Product added successfully", response });
+  } catch (error) {
+    res.status(500).json({ message: "Error at /product/add", error });
   }
 });
