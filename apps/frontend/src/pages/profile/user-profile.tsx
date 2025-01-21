b36c469efd52f97e8f570339abd7611c58aa3f60
Uday <udayraj.vadeghar@gmail.com>
2025-01-21 16:08:03 +0000

    add userUpdateForm , add more later

--- apps/frontend/src/pages/profile/user-profile.tsx
+++ apps/frontend/src/pages/profile/user-profile.tsx
@@ -1,3 +1,12 @@
+import { z } from "zod";
+
+const userUpdateForm = z.object({
+  username: z.string().min(2).max(6),
+  password: z.string().min(6, {
+    message: "The password should be atleast 6 characters long",
+  }),
+});
+
 const UserProfile = () => {
   return <div className="pt-20">UserProfile</div>;
 };
