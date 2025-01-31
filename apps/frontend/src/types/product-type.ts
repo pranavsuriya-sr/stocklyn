ec33a60bfce456e440000db0aa3db2c385debb8f
Uday <udayraj.vadeghar@gmail.com>
2025-01-31 08:03:20 +0000

    add product-category type

---
+++ apps/frontend/src/types/product-type.ts
@@ -0,0 +1,17 @@
+export interface ProductsType {
+  id: string;
+  name: string;
+  displayImage: string;
+  imageUrl: string[];
+  price: number;
+  productDescription: string;
+  categoryName: string;
+  colors: string[];
+  sizes: string[];
+  highlights: string[];
+  details: string;
+  reviews: number;
+  stockQuantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
