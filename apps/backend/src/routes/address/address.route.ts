d3ca846081d84734651baba0c9b368e5bc383b00
Uday <udayraj.vadeghar@gmail.com>
2025-02-07 11:52:44 +0000

    add address route for saving address

---
+++ apps/backend/src/routes/address/address.route.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from "express";
+import { prisma } from "../../server";
+
+const addressRoute = express.Router();
+
+addressRoute.post("/addAddress", async (req: Request, res: Response) => {
+  const {
+    name,
+    mobileNumber,
+    state,
+    pincode,
+    city,
+    Address1,
+    Address2,
+    Landmark,
+    userId,
+  } = req.body;
+
+  if (
+    !name ||
+    !mobileNumber ||
+    !state ||
+    !pincode ||
+    !city ||
+    !Address1 ||
+    !Address2 ||
+    !Landmark ||
+    !userId
+  ) {
+    console.log("Error at /address/addAddress , missing feilds");
+    res.status(500).send("Something went wrong!");
+    return;
+  }
+
+  try {
+    const isValidUser = await prisma.users.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!isValidUser) {
+      console.log("Error at /address/addAddress , invalid userId");
+      res.status(500).send("Something went wrong!");
+    }
+
+    const response = await prisma.location.create({
+      data: {
+        name,
+        mobileNumber,
+        state,
+        pincode,
+        city,
+        Address1,
+        Address2,
+        Landmark,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    res.status(201).json({ message: "Added the location", response });
+  } catch (error) {
+    console.log("Error at /address/addAddress", error);
+    res.status(500).send("Something went wrong!");
+  }
+});
+
+export default addressRoute;
