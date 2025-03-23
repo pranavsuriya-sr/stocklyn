18b5e4409ee6fba9c9b3cc4310c83cfb6e5fdf20
Uday <udayraj.vadeghar@gmail.com>
2025-03-23 14:27:58 +0000

    fix build issues

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    fs: {
+      allow: ["."],
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
 });
