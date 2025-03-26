be25a33d80d1849b76198aaf68e3fee415a4a636
Uday <udayraj.vadeghar@gmail.com>
2025-03-26 07:04:49 +0000

    add responsive at about page

--- apps/frontend/src/pages/about/about-page.tsx
+++ apps/frontend/src/pages/about/about-page.tsx
@@ -17,188 +17,205 @@ import onlineShopping from "../../assets/online-shopping.png";
 const About = () => {
   const navigate = useNavigate();
   return (
-    <section className="container mx-auto w-5/6 pt-20 font-montserrat ">
-      <div className="flex pt-16" style={{ height: "calc(-58px + 100vh)" }}>
-        <div className="flex-1 pt-16 pr-16">
-          <h1 className="  text-7xl font-bold" style={{ lineHeight: 1.15 }}>
-            Seamless <br></br>E-Commerce Experience
+    <section className="container mx-auto px-4 sm:px-6 lg:w-5/6 pt-10 md:pt-20 font-montserrat">
+      <div
+        className="flex flex-col lg:flex-row pt-8 lg:pt-16"
+        style={{ minHeight: "calc(-58px + 100vh)" }}
+      >
+        <div className="flex-1 pt-8 lg:pt-16 lg:pr-16 order-2 lg:order-1">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight sm:leading-snug">
+            Seamless <br className="hidden sm:block" />
+            E-Commerce Experience
           </h1>
-          <p className="my-4 text-lg text-gray-700">
+          <p className="my-4 text-base sm:text-lg text-gray-700">
             An e-commerce platform that combines the best features of top
             e-commerce sites like Amazon, Shopify, and eBay.
           </p>
           <Button
-            className="px-12 py-6 mt-2 text-lg text-white rounded-md "
+            className="px-8 sm:px-12 py-4 sm:py-6 mt-2 text-base sm:text-lg text-white rounded-md"
             variant={"indigo"}
             onClick={() => navigate("/signup")}
           >
             Start Shopping
           </Button>
         </div>
-        <div className="flex-1 pt-8">
+        <div className="flex-1 pt-8 order-1 lg:order-2">
           <img
             src={onlineShopping}
-            className="block"
+            className="block w-full max-w-md mx-auto lg:max-w-none"
             alt="Image showcasing people browsing products on a website"
           />
         </div>
       </div>
 
       <section className="py-8">
-        <h1 className="mb-24 font-bold text-center text-gray-800 text-7xl">
+        <h1 className="mb-12 md:mb-24 font-bold text-center text-gray-800 text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
           Tech Stack Used
         </h1>
-        <div className="grid grid-cols-2 gap-12">
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={react} className="w-24 h-auto mb-3" alt="React" />
-            <p className="text-xl font-medium text-gray-700">React</p>
-            <p className="w-1/2 mt-1">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 md:gap-12">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={react}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="React"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              React
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               React is a JavaScript library for building fast and interactive
               user interfaces.
             </p>
           </div>
-          {/* <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={nextjs} className="w-24 h-auto mb-3" alt="Next.js" />
-            <p className="text-xl font-medium text-gray-700">Next.js</p>
-            <p className="w-1/2 mt-1">
-              Next.js is a React framework for hybrid static & server rendering,
-              optimized for e-commerce sites.
-            </p>
-          </div> */}
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
               src={reactquery}
-              className="w-24 h-auto mb-3"
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
               alt="React Query"
             />
-            <p className="text-xl font-medium text-gray-700">React Query</p>
-            <p className="w-1/2 mt-1">
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              React Query
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               React Query helps manage and fetch data efficiently for e-commerce
               websites.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
               src={tailwind}
-              className="w-24 h-auto mb-3"
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
               alt="Tailwind CSS"
             />
-            <p className="text-xl font-medium text-gray-700">Tailwind CSS</p>
-            <p className="w-1/2 mt-1">
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Tailwind CSS
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Tailwind CSS helps create responsive and visually appealing
               e-commerce designs.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
-              src={zustand} // Ensure you import the image
-              className="w-24 h-auto mb-3"
+              src={zustand}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
               alt="Zustand"
             />
-            <p className="text-xl font-medium text-gray-700">Zustand</p>
-            <p className="w-1/2 mt-1">
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Zustand
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Zustand is a fast and scalable state management library for React.
             </p>
           </div>
-
-          {/* <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
-              src="../../assets/icons/socket-io.svg"
-              className="w-24 h-auto mb-3"
-              alt="Socket.io"
+              src={node}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Node.js"
             />
-            <p className="text-xl font-medium text-gray-700">Socket.io</p>
-            <p className="w-1/2 mt-1">
-              Socket.io enables real-time communication for features like live
-              notifications and chats.
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Node.js
             </p>
-          </div> */}
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={node} className="w-24 h-auto mb-3" alt="Node.js" />
-            <p className="text-xl font-medium text-gray-700">Node.js</p>
-            <p className="w-1/2 mt-1">
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Node.js powers the server-side logic and handles backend processes
               efficiently.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={express} className="w-24 h-auto mb-3" alt="Express.js" />
-            <p className="w-1/2 mt-1">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={express}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Express.js"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Express.js
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Express.js simplifies API development and backend services crucial
               for e-commerce platforms.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={docker} className="w-24 h-auto mb-3" alt="Docker" />
-            <p className="text-xl font-medium text-gray-700">Docker</p>
-            <p className="w-1/2 mt-1">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={docker}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Docker"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Docker
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Docker helps ensure consistency and scalability for deploying
               e-commerce apps.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={nginx} className="w-24 h-auto mb-3" alt="Nginx" />
-            <p className="text-xl font-medium text-gray-700">Nginx</p>
-            <p className="w-1/2 mt-1">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={nginx}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Nginx"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Nginx
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Nginx is used as a reverse proxy for efficient and secure delivery
               of e-commerce content.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={prisma} className="w-24 h-auto mb-3" alt="Prisma" />
-            <p className="w-1/2 mt-1">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={prisma}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Prisma"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Prisma
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Prisma is a powerful ORM for managing database operations
               efficiently in e-commerce apps.
             </p>
           </div>
-          {/* <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
-              src="../../assets/icons/redis.svg"
-              className="w-24 h-auto mb-3"
-              alt="Redis"
+              src={postgres}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Postgres"
             />
-            <p className="text-xl font-medium text-gray-700">Redis</p>
-            <p className="w-1/2 mt-1">
-              Redis provides fast, real-time caching and session management for
-              e-commerce websites.
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Postgres
             </p>
-          </div> */}
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={postgres} className="w-24 h-auto mb-3" alt="Postgres" />
-            <p className="text-xl font-medium text-gray-700">Postgres</p>
-            <p className="w-1/2 mt-1">
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Postgres is a robust database solution for storing product data,
               user info, and order details.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img src={supabase} className="w-24 h-auto mb-3" alt="Supabase" />
-            <p className="text-xl font-medium text-gray-700">Supbase</p>
-            <p className="w-1/2 mt-1">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+            <img
+              src={supabase}
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
+              alt="Supabase"
+            />
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              Supbase
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               Supabase was used for S3 storage and Postgres Database for
               e-commerce app.
             </p>
           </div>
-          {/* <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
-            <img
-              src="../../assets/icons/cloudinary.svg"
-              className="w-24 h-auto mb-3"
-              alt="Cloudinary"
-            />
-            <p className="text-xl font-medium text-gray-700">Cloudinary</p>
-            <p className="w-1/2 mt-1">
-              Cloudinary offers cloud-based image storage and optimization,
-              essential for e-commerce platforms.
-            </p>
-          </div> */}
-          <div className="flex flex-col items-center justify-center col-span-1 py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
+          <div className="flex flex-col items-center justify-center py-8 sm:py-12 text-center bg-gray-100 rounded-md bg-opacity-40 hover:bg-opacity-70">
             <img
               src={actions}
-              className="w-24 h-auto mb-3"
+              className="w-16 sm:w-20 md:w-24 h-auto mb-3"
               alt="GitHub Actions"
             />
-            <p className="text-xl font-medium text-gray-700">GitHub Actions</p>
-            <p className="w-1/2 mt-1">
+            <p className="text-lg sm:text-xl font-medium text-gray-700">
+              GitHub Actions
+            </p>
+            <p className="w-4/5 sm:w-3/4 md:w-1/2 mt-1 text-sm sm:text-base">
               GitHub Actions enables CI/CD pipelines, helping automate
               deployments and maintain e-commerce systems.
             </p>
