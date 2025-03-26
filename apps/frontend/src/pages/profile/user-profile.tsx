829238ed7664e043dde1d26b4d4267f4861acb4a
Uday <udayraj.vadeghar@gmail.com>
2025-03-26 07:28:24 +0000

    add responsiveness @user-profile

--- apps/frontend/src/pages/profile/user-profile.tsx
+++ apps/frontend/src/pages/profile/user-profile.tsx
@@ -10,10 +10,10 @@ const UserProfile = () => {
   return (
     <div className="pt-20 h-screen font-montserrat">
       <div className="p-4 mt-5 text-3xl flex justify-center items-center ">
-        <div className="text-4xl ">Your Account </div>
+        <div className="text-4xl">Your Account</div>
       </div>
       <div className="w-[90%] mx-auto pt-5">
-        <div className="grid grid-cols-3 gap-2 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <UserInfoCard
             name="Location"
             description="Edit Addresses for orders and gifts"
@@ -22,13 +22,13 @@ const UserProfile = () => {
           />
           <UserInfoCard
             name="Orders"
-            description="Track , Return or Buy again"
+            description="Track, Return or Buy again"
             imgUrl={orders}
             onClick={() => navigate("/orderHistory")}
           />
           <UserInfoCard
             name="Authentication"
-            description="Edit login , name , email , password"
+            description="Edit login, name, email, password"
             imgUrl={authentication}
             onClick={() => navigate("/secureUserAuth")}
           />
