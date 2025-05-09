6e30bf50b8e27afa2d1f904c8c46e4e861c9a3c8
Uday <udayraj.vadeghar@gmail.com>
2025-05-09 18:11:26 +0000

    refactor App component to use React.lazy for dynamic imports of pages, enhancing performance and code organization; update routing structure for improved readability and maintainability

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,29 +1,41 @@
+import { lazy, Suspense } from "react";
+
 import { Analytics } from "@vercel/analytics/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import About from "./pages/about/about-page";
-import PolicyPage from "./pages/about/shipping-policy-page";
-import Login from "./pages/auth/login-page";
-import SellerLogin from "./pages/auth/sellerAuth/seller-login-page";
-import SellerSignUp from "./pages/auth/sellerAuth/seller-signup-page";
-import SignUp from "./pages/auth/signup-page";
-import Cart from "./pages/cart/cart-page";
-import CheckOut from "./pages/checkout/checkout-page";
-import ContactPage from "./pages/contact/contact-page";
-import NotFoundPage from "./pages/error/error-page";
-import Home from "./pages/home/home-page";
-import LandingPage from "./pages/landing/landing-page";
-import LocationAdd from "./pages/location/location-add-page";
-import LocationsDisplayPage from "./pages/location/locations-display-page";
-import OrderHistory from "./pages/order/order-history-page";
-import ProductPage from "./pages/product/product-page";
-import UserProfile from "./pages/profile/user-profile";
-import ProtectedPage from "./pages/protected/procted-page";
-import SearchPage from "./pages/search/search-page";
-import ShopPage from "./pages/shop/shop-page";
-import PaymentFailurePage from "./pages/stripe-redirect/failure-page";
-import PaymentSuccessPage from "./pages/stripe-redirect/success-page";
+const About = lazy(() => import("./pages/about/about-page"));
+const PolicyPage = lazy(() => import("./pages/about/shipping-policy-page"));
+const Login = lazy(() => import("./pages/auth/login-page"));
+const SellerLogin = lazy(
+  () => import("./pages/auth/sellerAuth/seller-login-page")
+);
+const SellerSignUp = lazy(
+  () => import("./pages/auth/sellerAuth/seller-signup-page")
+);
+const SignUp = lazy(() => import("./pages/auth/signup-page"));
+const Cart = lazy(() => import("./pages/cart/cart-page"));
+const CheckOut = lazy(() => import("./pages/checkout/checkout-page"));
+const ContactPage = lazy(() => import("./pages/contact/contact-page"));
+const NotFoundPage = lazy(() => import("./pages/error/error-page"));
+const Home = lazy(() => import("./pages/home/home-page"));
+const LandingPage = lazy(() => import("./pages/landing/landing-page"));
+const LocationAdd = lazy(() => import("./pages/location/location-add-page"));
+const LocationsDisplayPage = lazy(
+  () => import("./pages/location/locations-display-page")
+);
+const OrderHistory = lazy(() => import("./pages/order/order-history-page"));
+const ProductPage = lazy(() => import("./pages/product/product-page"));
+const UserProfile = lazy(() => import("./pages/profile/user-profile"));
+const ProtectedPage = lazy(() => import("./pages/protected/procted-page"));
+const SearchPage = lazy(() => import("./pages/search/search-page"));
+const ShopPage = lazy(() => import("./pages/shop/shop-page"));
+const PaymentFailurePage = lazy(
+  () => import("./pages/stripe-redirect/failure-page")
+);
+const PaymentSuccessPage = lazy(
+  () => import("./pages/stripe-redirect/success-page")
+);
 
 const pageVariants = {
   initial: {
@@ -56,29 +68,187 @@ const App = () => {
   return (
     <>
       {/* {loading && <LoadingPage />} */}
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route
-            path="/userProfile"
-            element={
-              <ProtectedPage>
-                <motion.div
-                  initial="initial"
-                  animate="in"
-                  // exit="out"
+      <Suspense>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/userProfile"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    // exit="out"
 
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <UserProfile />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            />
+            <Route
+              path="/cartItems"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <Cart />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            />
+            <Route
+              path="/shop"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <ShopPage />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            />
+            <Route
+              path="/searchResults/:id"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <SearchPage />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            />
+            <Route
+              path="/editUserAddress"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <LocationsDisplayPage />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/addUserAddress"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <LocationAdd />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/success"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <PaymentSuccessPage />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/failure"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <PaymentFailurePage />
+                  </motion.div>
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/orderHistory"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <OrderHistory />
+                  </motion.div>{" "}
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedPage>
+                  <motion.div
+                    initial="initial"
+                    animate="in"
+                    exit="out"
+                    variants={pageVariants}
+                    transition={pageTransition}
+                  >
+                    <CheckOut />
+                  </motion.div>{" "}
+                </ProtectedPage>
+              }
+            ></Route>
+            <Route
+              path="/contact"
+              element={
+                <motion.div
+                  initial="initial"
+                  animate="in"
+                  exit="out"
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <UserProfile />
+                  <ContactPage />
                 </motion.div>
-              </ProtectedPage>
-            }
-          />
-          <Route
-            path="/cartItems"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/home"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -86,15 +256,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <Cart />
+                  <Home />
                 </motion.div>
-              </ProtectedPage>
-            }
-          />
-          <Route
-            path="/shop"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/about"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -102,15 +270,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <ShopPage />
+                  <About />
                 </motion.div>
-              </ProtectedPage>
-            }
-          />
-          <Route
-            path="/searchResults/:id"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -118,15 +284,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <SearchPage />
+                  <LandingPage />
                 </motion.div>
-              </ProtectedPage>
-            }
-          />
-          <Route
-            path="/editUserAddress"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/login"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -134,15 +298,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <LocationsDisplayPage />
+                  <Login />
                 </motion.div>
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/addUserAddress"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -150,15 +312,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <LocationAdd />
+                  <SignUp />
                 </motion.div>
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/success"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/product/:productId"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -166,15 +326,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <PaymentSuccessPage />
+                  <ProductPage />
                 </motion.div>
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/failure"
-            element={
-              <ProtectedPage>
+              }
+            />
+            <Route
+              path="/policy"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -182,15 +340,14 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <PaymentFailurePage />
+                  <PolicyPage />
                 </motion.div>
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/orderHistory"
-            element={
-              <ProtectedPage>
+              }
+            />
+            //Seller Routes
+            <Route
+              path="/seller/signup"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -198,15 +355,13 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <OrderHistory />
-                </motion.div>{" "}
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedPage>
+                  <SellerSignUp />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/seller/login"
+              element={
                 <motion.div
                   initial="initial"
                   animate="in"
@@ -214,168 +369,27 @@ const App = () => {
                   variants={pageVariants}
                   transition={pageTransition}
                 >
-                  <CheckOut />
-                </motion.div>{" "}
-              </ProtectedPage>
-            }
-          ></Route>
-          <Route
-            path="/contact"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <ContactPage />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/home"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <Home />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <About />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <LandingPage />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <Login />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <SignUp />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/product/:productId"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <ProductPage />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/policy"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <PolicyPage />
-              </motion.div>
-            }
-          />
-          //Seller Routes
-          <Route
-            path="/seller/signup"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <SellerSignUp />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/seller/login"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <SellerLogin />
-              </motion.div>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <motion.div
-                initial="initial"
-                animate="in"
-                exit="out"
-                variants={pageVariants}
-                transition={pageTransition}
-              >
-                <NotFoundPage />
-              </motion.div>
-            }
-          />
-        </Routes>
-      </AnimatePresence>
+                  <SellerLogin />
+                </motion.div>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <motion.div
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                  variants={pageVariants}
+                  transition={pageTransition}
+                >
+                  <NotFoundPage />
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
+      </Suspense>
       <Analytics />
     </>
   );
