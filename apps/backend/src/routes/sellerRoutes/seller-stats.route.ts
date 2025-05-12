9f81235eee437c579156a624dc63c701d5fa46e8
Uday <udayraj.vadeghar@gmail.com>
2025-05-12 19:59:02 +0000

    update response to provide comprehensive seller statistics.

--- apps/backend/src/routes/sellerRoutes/seller-stats.route.ts
+++ apps/backend/src/routes/sellerRoutes/seller-stats.route.ts
@@ -19,18 +19,27 @@ sellerStatsRoute.get(
       where: { sellerId: sellerId },
     });
 
-    res.status(200).json({ totalProducts, totalOrders });
+    const totalItemsSold = await prisma.orderItems.aggregate({
+      where: { sellerId: sellerId },
+      _sum: {
+        quantity: true,
+      },
+    });
+
+    const totalCountOfItemsSold = totalItemsSold._sum.quantity;
+
+    res.status(200).json({ totalProducts, totalOrders, totalCountOfItemsSold });
   }
 );
 
 // sellerStatsRoute.post(
 //   "/sellerStats/:sellerId",
 //   async (req: Request, res: Response) => {
-//     const sellerId = "";
+//     const sellerId = "cmai1dmbs0005wvr36bcyz2hw";
 
 //     const resposne = await prisma.orderItems.updateMany({
 //       where: {
-//         sellerId: null,
+//         sellerId: sellerId,
 //       },
 //       data: {
 //         sellerId: sellerId,
