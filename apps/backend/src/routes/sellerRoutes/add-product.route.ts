b92488d9b5e6077ab52631ee6b77031185622d2a
Uday <udayraj.vadeghar@gmail.com>
2025-05-24 21:03:04 +0000

    Integrate Supabase storage for image uploads in add-product route, enhancing file handling and response with public URL.

--- apps/backend/src/routes/sellerRoutes/add-product.route.ts
+++ apps/backend/src/routes/sellerRoutes/add-product.route.ts
@@ -1,9 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
 import express, { Request, Response } from "express";
 import multer from "multer";
 
 const addProductRoute = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 addProductRoute.post(
   "/uploadImage",
@@ -11,12 +15,35 @@ addProductRoute.post(
   async (req, res) => {
     //@ts-ignore
     const file = req.file;
+    // const filePath = `product.images/${file.originalname}`;
     console.log(file);
-    res.send({ file: file.path });
+
+    try {
+      const { data, error } = await supabase.storage
+        .from("product.images")
+        .upload(file.originalname, file.buffer, {
+          contentType: file.mimetype,
+          upsert: true,
+        });
+
+      if (error) {
+        res.send(error);
+      }
+
+      const { data: publicUrl } = supabase.storage
+        .from("product.images")
+        .getPublicUrl(file.originalname);
+
+      //   console.log(publicUrl);
+      console.log(publicUrl.publicUrl);
+      res.status(200).send({ publicUrl: publicUrl.publicUrl });
+    } catch (error) {
+      res.send(error);
+    }
   }
 );
 
-addProductRoute.get("/testRoute", async (req: Request, res: Response) => {
+addProductRoute.post("/InsertProduct", async (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
