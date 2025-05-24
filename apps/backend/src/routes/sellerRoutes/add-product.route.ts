0bc9ebacbd56984512555f9894d8a78abb8ed302
Uday <udayraj.vadeghar@gmail.com>
2025-05-24 20:20:14 +0000

    add  sellerRoutes by integrating multer for file handling and updating response structure to include file path.

--- apps/backend/src/routes/sellerRoutes/add-product.route.ts
+++ apps/backend/src/routes/sellerRoutes/add-product.route.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
+import multer from "multer";
 
 const addProductRoute = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-addProductRoute.post("/uploadImage", async (req: Request, res: Response) => {
-  const { name, description, price, category, sellerId } = req.body;
-});
+addProductRoute.post(
+  "/uploadImage",
+  upload.single("file"),
+  async (req, res) => {
+    //@ts-ignore
+    const file = req.file;
+    console.log(file);
+    res.send({ file: file.path });
+  }
+);
 
 addProductRoute.get("/testRoute", async (req: Request, res: Response) => {
   res.send("Hello World");
