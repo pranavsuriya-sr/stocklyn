0a8998ad86fd675302cf82999fbac00cc29f98f9
Uday <udayraj.vadeghar@gmail.com>
2025-06-05 11:46:55 +0000

    Add withCredentials option to all API routes for improved session management

--- apps/frontend/src/api/api.ts
+++ apps/frontend/src/api/api.ts
@@ -34,39 +34,46 @@ export const sendEmailRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_SEND_EMAIL_ROUTE_PROD
     : import.meta.env.VITE_SEND_EMAIL_ROUTE,
+  withCredentials: true,
 });
 
 export const addressRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_ADDRESS_ROUTE_PROD
     : import.meta.env.VITE_ADDRESS_ROUTE,
+  withCredentials: true,
 });
 export const paymentRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_PAYMENT_ROUTE_PROD
     : import.meta.env.VITE_PAYMENT_ROUTE,
+  withCredentials: true,
 });
 
 export const orderRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_ORDER_ROUTE_PROD
     : import.meta.env.VITE_ORDER_ROUTE,
+  withCredentials: true,
 });
 
 export const sellerStatsRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_SELLER_STATS_ROUTE_PROD
     : import.meta.env.VITE_SELLER_STATS_ROUTE,
+  withCredentials: true,
 });
 
 export const addProductRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_ADD_PRODUCT_ROUTE_PROD
     : import.meta.env.VITE_ADD_PRODUCT_ROUTE,
+  withCredentials: true,
 });
 
 export const adminRoute = axios.create({
   baseURL: isProd
     ? import.meta.env.VITE_ADMIN_ROUTE_PROD
     : import.meta.env.VITE_ADMIN_ROUTE,
+  withCredentials: true,
 });
